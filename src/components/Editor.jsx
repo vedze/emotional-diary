ceabcbadc39bb498e88c18cf53b556bbad79800e
vedze <ezdev00@gmail.com>
2024-05-22 21:40:22 +0900

    Fix: Edit 페이지 기능 구현
    - Header에 삭제 버튼 구현
    - 해당하는 ID를 갖는 일기 데이터 가져와 initializing
    - window.confirm으로 수정 및 삭제 시 한 번 더 확인
    - 존재하지 않는 ID의 일기에 접근 시 alert로 알린 후 home으로 navigate

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const emotionList = [
@@ -28,13 +28,24 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
   const nav = useNavigate();
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
     content: "",
   });
+
+  // 수정 시 받아오는 initData로 initializing
+  useEffect(() => {
+    // initData가 존재한다면 Input데이터를 initData로 설정
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+    }
+  }, [initData]);
 
   const onChangeInput = (e) => {
     let name = e.target.name; // 어떤 요소에 입력이 들어왔는지
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,9 +1,64 @@
-import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 
 const Edit = () => {
+  const nav = useNavigate();
   const params = useParams();
+  const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+  const [currentDiaryItem, setCurrentDiaryItem] = useState();
 
-  return <div>{params.id}번 일기 수정페이지</div>;
+  // mount시, params.id나 data 변경 시 실행
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+
+    setCurrentDiaryItem(currentDiaryItem);
+  }, [params.id, data]);
+
+  const onClickDelete = () => {
+    // 한 번 더 확인하는 팝업
+    if (window.confirm("정말 삭제할까요? 한 번 삭제되면 복구할 수 없어요")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        text={"일기 수정하기"}
+        leftChild={<Button text={"< 뒤로가기"} onClick={() => nav(-1)} />}
+        rightChild={
+          <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
+        }
+      />
+      <Editor initData={currentDiaryItem} onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Edit;
