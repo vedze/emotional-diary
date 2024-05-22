05fee40e199be9c0f9c08ca3d24890afaa269e3f
vedze <ezdev00@gmail.com>
2024-05-22 19:14:54 +0900

    Fix: New 페이지 기능 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,6 +2,9 @@ import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const emotionList = [
   { emotionId: 1, emotionName: "아주 좋음" },
   { emotionId: 2, emotionName: "좋음" },
@@ -10,14 +13,58 @@ const emotionList = [
   { emotionId: 5, emotionName: "아주 나쁨" },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+const getStringedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const nav = useNavigate();
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const onChangeInput = (e) => {
+    let name = e.target.name; // 어떤 요소에 입력이 들어왔는지
+    let value = e.target.value; // 입력된 값
+
+    // 날짜인 경우는 문자열이 아닌 Date 객체가 필요하기 때문에 변환
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
@@ -26,18 +73,39 @@ const Editor = () => {
             <EmotionItem
               key={item.emotionId}
               {...item}
-              isSelected={item.emotionId === emotionId}
+              // input 이나 select, textarea 등이 아닌 그저 컴포넌트들의 나열이기 때문에
+              // 클릭 시 이벤트를 직접 만들어줘야 함, 이벤트 객체를 직접 만들어서 전달해야 함
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                })
+              }
+              isSelected={item.emotionId === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+        />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button text={"취소하기"} onClick={() => nav(-1)} />
+        {/* Editor 컴포넌트는 Edit와 new 페이지 두 군데에서 사용되기 때문에 그냥 onCreate나 onUpdate를 실행해버리면 안 됨 */}
+        {/* Editor 컴포넌트에서 context로부터 직접 받지 않고, 작성완료 버튼을 눌렀을 때 실행돼야 하는 함수를 props로 전달해주면 됨 */}
+        <Button
+          text={"작성완료"}
+          type={"POSITIVE"}
+          onClick={onClickSubmitButton}
+        />
       </section>
     </div>
   );
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,15 +1,35 @@
+import { useContext } from "react";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
 
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
+
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const onSubmit = (input) => {
+    // createdDate는 타임스탬프 형식으로 가져오기
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로가기"} />}
+        leftChild={
+          <Button
+            text={"< 뒤로가기"}
+            onClick={() => {
+              nav(-1);
+            }}
+          />
+        }
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
