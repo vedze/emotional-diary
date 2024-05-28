e0091c301e4a23f927b4b9d689d545b41b28beac
vedze <ezdev00@gmail.com>
2024-05-28 22:32:31 +0900

    Fix: Title, Favicon, og 설정
    - 페이지 별 타이틀 설정하는 별도의 훅 생성해 각 페이지 타이틀 설정
    - Favicon, 오픈 그래프 설정 완료

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,14 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
 
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
 
 const New = () => {
   const nav = useNavigate();
   const { onCreate } = useContext(DiaryDispatchContext);
+  usePageTitle("새 일기 작성하기");
 
   const onSubmit = (input) => {
     // createdDate는 타임스탬프 형식으로 가져오기
